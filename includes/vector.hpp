e3bbae1eb537406e3496a10eeb4db14fd87aeccb
lollith <adeline.gouet@gmail.com>
2022-12-27 18:04:36 +0000

    debut

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lollith <lollith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 13:31:04 by agouet            #+#    #+#             */
-/*   Updated: 2022/12/22 18:16:33 by agouet           ###   ########.fr       */
+/*   Updated: 2022/12/27 18:04:06 by lollith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,24 @@
 # include <iostream>
 
 namespace ft {
+	//allocator = class
 	template < typename T, typename Alloc = std::allocator<T> >// si pas de precision Alloc va compiler comme type T de la classe std::allocator
 	class Vector{
 		public:
 //---------------------------------------------constructor----------------------
-//(1)empty 
+//(1)empty
+ Vector( void );
 
 //(2)fill
-
+Vector( size_t n );
 //(3)range
-
+Vector ( T first, T last);
 //(4)copy
-	
+Vector (Vector const &copy);
+//destructeur
+~Vector( void );
+//operator=
+
 //---------------------------------------------members fct----------------------
 
 //---------------------------------------------overloards-----------------------
@@ -37,16 +43,18 @@ namespace ft {
 //---------------------------------------------iterators------------------------
 
 		private:
-		T 														_value_type;
-		Alloc													_allocator_type;
-		_allocator_type::reference								_reference;
+		typedef T 														_value_type;
+		typedef Allocator													_allocator_type;
+		typedef typename _allocator_type::reference				_reference;
 		typedef typename _allocator_type::const_reference 		_const_reference; //typename rappeler pour pas que le compilateur se perde // type def =alias ,evite les using 
-		_allocator_type::pointer								_pointer;
-		_allocator_type::const_pointer							_const_pointer;
-		ptrdiff_t												_diference_type;
-		size_t													_size_type;
+		typedef typename _allocator_type::pointer				_pointer;
+		typedef typename _allocator_type::const_pointer			_const_pointer;
+		typedef std::ptrdiff_t												_diference_type;
+		typedef std::size_t													_size_type;
 	//iterators??
-		
+	
+		//The container keeps an internal copy of alloc, which is used to allocate storage throughout its lifetime.
+		Allocator		_alloc;
 		
 		
 	
--- includes/vector.tpp
+++ includes/vector.tpp
@@ -3,10 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   vector.tpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lollith <lollith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 13:31:53 by agouet            #+#    #+#             */
-/*   Updated: 2022/12/22 14:35:18 by agouet           ###   ########.fr       */
+/*   Updated: 2022/12/27 18:04:09 by lollith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+template<typename T, typename Alloc>
+Vector<T>::Vector() :_alloc(Allocator())
+{}
+
+
+template<typename T, typename Alloc>
+Vector<T>::Vector( size_t n){}
