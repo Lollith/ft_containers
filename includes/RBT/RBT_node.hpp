637167aa3d852065d18dcedfcc3507b692df68c1
Adeline GOUET <agouet@e1r8p19.clusters.42paris.fr>
2023-01-20 18:17:26 +0100

    new node

--- includes/RBT/RBT_node.hpp
+++ includes/RBT/RBT_node.hpp
@@ -6,7 +6,7 @@
 /*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 10:39:24 by agouet            #+#    #+#             */
-/*   Updated: 2023/01/20 17:30:17 by agouet           ###   ########.fr       */
+/*   Updated: 2023/01/20 18:17:06 by agouet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,51 @@ namespace ft {
 	}			t_color;
 
 
-	template < class T, class Allocator = std::allocator<T> >
-	class RBT_node
+	// template < class T, class Allocator = std::allocator<T> >
+	// class RBT_node
+	// {
+	// 	private:
+	// 		T				_value;
+	// 		t_color			_color;
+	// 		RBT_node		*_left;
+	// 		RBT_node		*_right;
+	// 		RBT_node		*_parent;
+	// 		Allocator		_alloc;
+		
+	// 	public:
+	// 		RBT_node( const Allocator &alloc = Allocator() ); // si RBT_node( void ) => Alloc par default
+	// 		~RBT_node( void );
+	// 		RBT_node(RBT_node const &copy);
+	// 		RBT_node &operator=(const RBT_node &rhs);
+
+	// 	//surcharge
+	// 		RBT_node(const T &value, t_color color, RBT_node *left, RBT_node *right, 
+	// 			RBT_node *parent, const Allocator &alloc = Allocator());
+
+
+
+// In each node, there is a boolean value x.leaf which is true if x is a leaf.
+// https://www.programiz.com/dsa/b-tree
+	template < class Key, class T >
+	struct RBT_node
 	{
-		private:
-			T				_value;
+			typedef pair< const Key, T>		value_type;
+// voir mamhed
+	
+			// T				_value;
 			t_color			_color;
+			RBT_node		*_parent;
 			RBT_node		*_left;
 			RBT_node		*_right;
-			RBT_node		*_parent;
-			Allocator		_alloc;
-		
-		public:
-			RBT_node( const Allocator &alloc = Allocator() ); // si RBT_node( void ) => Alloc par default
-			~RBT_node( void );
-			RBT_node(RBT_node const &copy);
-			RBT_node &operator=(const RBT_node &rhs);
+			value_type		pair_data;
+			bool 			leaf;
 
-		//surcharge
-			RBT_node(const T &value, t_color color, RBT_node *left, RBT_node *right, 
-				RBT_node *parent, const Allocator &alloc = Allocator());
+			RBT_node( void );
+			RBT_node( value_type pair_init );
+			~RBT_node( void );
+
+
+	
 
 	};
 	
